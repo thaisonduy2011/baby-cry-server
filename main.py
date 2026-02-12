1dc69a033626414d276e8966fdeef36121478d26
thaisonduy2011 <thaisonduy2011@gmail.com>
2026-02-12 09:39:54 +0700

    create sv

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request
+import requests
+import os
+
+app = FastAPI()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+@app.get("/")
+def home():
+    return {"status": "Server is running"}
+
+@app.post("/alert")
+async def alert(request: Request):
+    data = await request.json()
+    message = data.get("message", "Baby Cry Detected!")
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": message
+    })
+
+    return {"success": True}
